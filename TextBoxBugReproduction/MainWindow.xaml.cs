c4c99d506677bc97f8884906930ccdb31737be99
Tom Huibers <TomHuibers@hillstar.nl>
2020-06-02 11:21:27 +0200

    Add project files.

---
+++ TextBoxBugReproduction/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace TextBoxBugReproduction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UserControl
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabablzControl_SelectionChanged( object sender,
+                                                      SelectionChangedEventArgs e )
+        {
+          
+        }
+    }
+}
